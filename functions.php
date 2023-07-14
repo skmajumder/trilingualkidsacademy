65bf0d8b7c73b4815ab27b88ef90f9c819438513
skmajumder <skmajumder.cse@gmail.com>
2023-07-14 22:03:03 +0600

    Theme support enabel

--- functions.php
+++ functions.php
@@ -10,3 +10,13 @@ define( 'VERSION', wp_get_theme()->get( 'Version' ) );
  * Active TGM Plugin
  */
 require_once get_theme_file_path( './inc/tgm.php' );
+
+/**
+ * Enable trilingualkidsacademy Theme Support
+ */
+
+require_once get_theme_file_path( './inc/functions/theme-support.php' );
+
+/**
+ * trilingualkidsacademy theme assets
+ */
---
+++ inc/functions/theme-support.php
@@ -0,0 +1,34 @@
+<?php
+if ( ! function_exists( 'trilingualkidsacademy_theme_support' ) ):
+	function trilingualkidsacademy_theme_support(): void {
+		load_theme_textdomain( 'trilingualkidsacademy' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'wp-block-styles' );
+		add_editor_style( './assets/css/editor-style.css' );
+
+		/**
+		 * Enable HTML5
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+			'comment-list',
+			'comment-form',
+			'navigation-widgets'
+		) );
+
+		/**
+		 * Menu Register
+		 */
+		register_nav_menus( array(
+			'topMenuLocation'    => esc_html__( 'Top Menu', 'trilingualkidsacademy' ),
+			'footerMenuLocation' => esc_html__( 'Footer Menu', 'trilingualkidsacademy' ),
+		) );
+	}
+
+	add_action( 'after_setup_theme', 'trilingualkidsacademy_theme_support' );
+endif;
